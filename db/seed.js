674e045782f94d57a92b5b58c2a606bf45332380
kwaxite <146014461+kwaxite@users.noreply.github.com>
2024-02-13 17:50:36 +0000

    Add test data and update  readme file

---
+++ db/seed.js
@@ -0,0 +1,28 @@
+const db = require("./connection");
+
+function seed({ parks, rides, stalls }) {
+  return db
+    .query("DROP TABLE IF EXISTS rides;")
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS stalls;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS foods;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS stalls_foods;");
+    })
+    .then(() => {
+      return db.query("DROP TABLE IF EXISTS parks;");
+    })
+    .then(() => {
+      return createParks();
+    });
+}
+
+function createParks() {
+  /* Create your parks table in the query below */
+  return db.query("");
+}
+
+module.exports = seed;
